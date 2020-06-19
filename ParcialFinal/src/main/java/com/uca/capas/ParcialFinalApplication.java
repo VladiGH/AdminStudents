4bb888748000bd695edc666a32bfb59650b04ce2
VladiGH <00103017@uca.edu.sv>
2020-06-19 14:54:05 -0600

    Base project

---
+++ ParcialFinal/src/main/java/com/uca/capas/ParcialFinalApplication.java
@@ -0,0 +1,13 @@
+package com.uca.capas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParcialFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParcialFinalApplication.class, args);
+	}
+
+}
---
+++ ParcialFinal/src/test/java/com/uca/capas/ParcialFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uca.capas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParcialFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
