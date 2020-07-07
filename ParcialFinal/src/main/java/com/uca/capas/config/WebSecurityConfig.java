87c6707e7d4e8beb646e6ffcb08dd990fd165a1d
Alexis Gómez <00122717@uca.edu.sv>
2020-07-07 13:56:37 -0600

    :sparkles: Añadir verificacion de otros usuarios dentro del sistema.

--- ParcialFinal/src/main/java/com/uca/capas/config/WebSecurityConfig.java
+++ ParcialFinal/src/main/java/com/uca/capas/config/WebSecurityConfig.java
@@ -6,8 +6,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.session.SessionRegistry;
+import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
@@ -25,5 +28,16 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         provider.setUserDetailsService(userDetailsService);
         provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
         return provider;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.sessionManagement().maximumSessions(1).sessionRegistry(sessionRegistry());
+        super.configure(http);
+    }
+
+    @Bean
+    public SessionRegistry sessionRegistry() {
+        return new SessionRegistryImpl();
     }
 }
