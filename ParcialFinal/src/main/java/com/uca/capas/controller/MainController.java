f9576e4b5f6ab3116f3921a6f2a4810defeb3f48
Alexis Gómez <00122717@uca.edu.sv>
2020-07-09 21:34:29 -0600

    :sparkles: Añadir registro

--- ParcialFinal/src/main/java/com/uca/capas/controller/MainController.java
+++ ParcialFinal/src/main/java/com/uca/capas/controller/MainController.java
@@ -1,15 +1,34 @@
 package com.uca.capas.controller;
 
+import com.uca.capas.domain.Municipio;
+import com.uca.capas.domain.Rol;
+import com.uca.capas.domain.UsuarioSistema;
+import com.uca.capas.service.MunicipioService;
+import com.uca.capas.service.RolService;
+import com.uca.capas.service.UsuarioSistemaService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.Valid;
+import java.util.List;
 
 
 @Controller
 public class MainController {
+
+    @Autowired
+    MunicipioService municipioService;
+
+    @Autowired
+    UsuarioSistemaService userService;
+
+    @Autowired
+    RolService rolService;
 
     @RequestMapping("/index")
     public ModelAndView inicio() {
@@ -32,6 +51,58 @@ public class MainController {
         ModelAndView mav = new ModelAndView();
         mav.addObject("errorMsg","Ya hay un usuario logeado en el sistema");
         mav.setViewName("/login");
+        return mav;
+    }
+
+    @GetMapping("register")
+    public ModelAndView inicio3() {
+        ModelAndView mav = new ModelAndView();
+        List<Municipio> municipiosL = null;
+        try {
+            municipiosL = municipioService.findAll();
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
+        List<Rol> rolesL = null;
+        try{
+            rolesL = rolService.findAll();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        mav.setViewName("register");
+        mav.addObject("municipios", municipiosL);
+        mav.addObject("roles",rolesL);
+        mav.addObject("user", new UsuarioSistema());
+        return mav;
+    }
+
+    @PostMapping("register")
+    public ModelAndView saveUser(@Valid @ModelAttribute UsuarioSistema user, BindingResult result) {
+        System.out.println("hola");
+        ModelAndView mav = new ModelAndView();
+        if(result.hasErrors()) {
+            List<Municipio> municipiosL = null;
+            try {
+                municipiosL = municipioService.findAll();
+            }catch(Exception e) {
+                e.printStackTrace();
+            }
+            mav.addObject("municipios", municipiosL);
+            mav.addObject("user", user);
+            mav.addObject("respuesta", "Hubo un error al guardar, revisar campos");
+            mav.setViewName("register");
+        }else {
+            user.setEstado(false);
+            userService.save(user);
+            List<UsuarioSistema> usuarios = null;
+            try {
+                usuarios = userService.findAll();
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+            mav.addObject("msg","Registro realizado con exito!");
+            mav.setViewName("login");
+        }
         return mav;
     }
 
